59c6224a1505f11dd0a7fa9ea55c524b871efd7b
Jose Elias Ribeiro Moreira <jmoreira@viveendo.com.br>
2021-08-15 12:59:06 -0300

    ajustes

--- tf-module/eks/vpc.tf
+++ tf-module/eks/vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "gitops-lab"
 }
 
 resource "random_string" "suffix" {
